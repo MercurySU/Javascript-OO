d9aa99c6dec5cda6bb5e4db748dfa7244344f07c
Suzana <suzana@adxspace.com.br>
2023-01-30 14:57:49 -0300

    objeto em  javascript

---
+++ obj-literal.js
@@ -0,0 +1,11 @@
+const userHBO = {
+    name: "Suzana",
+    sobrenome:"Souza Araujo",
+    email:"suzana@adxspace.com.br",
+    conta: true,
+    exibeInfo : function(){
+        console.log(this.name, this.email)
+    }
+}
+userHBO.exibeInfo()
+
