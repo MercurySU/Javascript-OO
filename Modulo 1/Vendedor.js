d1aec90e1e6a8593c1936bcb4e3db73106a3addc
Suzana <suzana@adxspace.com.br>
2023-03-10 14:54:05 -0300

    tirei o extends de carro

--- Modulo 1/Vendedor.js
+++ Modulo 1/Vendedor.js
@@ -1,8 +1,12 @@
 import Carro from "./Carro.js"
 
-class Vendedor extends Carro {
-    constructor(name, cpf, ){
-        //ver erros de unexpected keyword
+class Vendedor {
+    constructor(name, cpf){
+        this.name = name
+        this.cpf = cpf
     }
 }
 
+const novoVendedor = new Vendedor('camila','08398298')
+console.log(novoVendedor)
+ 
